225e0e05088962fbebab9d8578843b671b7ad5c3
Soraku7 <2673140287@qq.com>
2025-01-30 02:01:35 +0800

    Scence Manager

---
+++ Assets/Scripts/Manager/ScenceManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Base;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Manager
+{
+    public class ScenceManager : SingletonMono<ScenceManager>
+    {
+        [SerializeField]
+        public List<GameObject> scenceObjects = new List<GameObject>();
+
+
+        private void Update()
+        {
+            
+        }
+    }
+}
